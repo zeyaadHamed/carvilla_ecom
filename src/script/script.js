9ee925005051da60794be125f26f3ad69e9ce5bf
ZiadHamed <zeyadhamed2002@gmail.com>
2024-08-31 01:37:51 +0300

    E-Commarce

--- src/script/script.js
+++ src/script/script.js
@@ -1,9 +1,20 @@
 "use strict";
-let btn = document.getElementById("btn");
-let linksScroll = document.querySelector('.links_scroll');
+const btn = document.getElementById("btn");
+const linksScroll = document.querySelector('.links_scroll');
+const links = document.querySelectorAll('.links_scroll .link');
+const arrowUp = document.querySelector('.arrow_up');
+const yearElement = document.getElementById('year');
 let isScrolledUp = false;
-let links = document.querySelectorAll('.links_scroll .link');
-function setupToggleBar() {
+const toggleBar = () => {
+    if (isScrolledUp) {
+        linksScroll.style.maxHeight = "0px";
+    }
+    else {
+        linksScroll.style.maxHeight = `${linksScroll.scrollHeight}px`;
+    }
+    isScrolledUp = !isScrolledUp;
+};
+const setupToggleBar = () => {
     if (window.matchMedia("(max-width: 834px)").matches) {
         btn.addEventListener("click", toggleBar);
         linksScroll.style.maxHeight = "0px";
@@ -12,29 +23,38 @@ function setupToggleBar() {
         btn.removeEventListener("click", toggleBar);
         linksScroll.style.maxHeight = "none";
     }
-}
-function toggleBar() {
-    if (isScrolledUp) {
-        linksScroll.style.maxHeight = "0px";
+};
+const checkScrollPosition = () => {
+    if (window.scrollY >= 450) {
+        arrowUp.style.display = 'flex';
     }
     else {
-        linksScroll.style.maxHeight = `${linksScroll.scrollHeight}px`;
+        arrowUp.style.display = 'none';
     }
-    isScrolledUp = !isScrolledUp;
-}
-setupToggleBar();
-window.addEventListener('resize', setupToggleBar);
-links.forEach((link) => {
-    link.addEventListener("click", () => {
-        if (link.classList.contains('active')) {
-            link.classList.remove('active');
-        }
-        else {
+};
+const handleActiveLink = () => {
+    links.forEach(link => {
+        link.addEventListener("click", () => {
             links.forEach(l => l.classList.remove('active'));
             link.classList.add('active');
-        }
+        });
+    });
+};
+const setCurrentYear = () => {
+    const currentYear = new Date().getFullYear();
+    yearElement.textContent = currentYear.toString();
+};
+window.addEventListener('load', () => {
+    setupToggleBar();
+    checkScrollPosition();
+    setCurrentYear();
+    handleActiveLink();
+});
+window.addEventListener('resize', setupToggleBar);
+document.addEventListener('scroll', checkScrollPosition);
+arrowUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
     });
 });
-let year = document.getElementById('year');
-let years = new Date().getFullYear();
-year.textContent = years.toString();
